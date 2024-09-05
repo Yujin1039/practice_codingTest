c28a7dbdb6d44c541c0cc84349f42da0bb40f7a4
송유진 <140707732+Yujin1039@users.noreply.github.com>
2024-09-06 00:05:12 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 100 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,31 +1,20 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {    
-    static int croatia(String[] cre, int ver){
-        int minus = 0;
-        if(cre.length > 1){
-            minus = cre.length-1;
-            for(int i=0;i<cre.length-1;i++){
-                int len = cre[i].length();
-                if(ver == 1 && len > 1 && cre[i].charAt(len-1) == 'z' && cre[i].charAt(len-2) == 'd'){
-                    minus += 1;
-                }  
-                if(ver == 2 && len > 0 && cre[i].charAt(len-1) != 'l' && cre[i].charAt(len-1) != 'n'){
-                    minus -= 1;
-                }
-                if(ver == 2 && len == 0) minus -= 1;
-            }
-        }
-        return minus;
-    }
-    
+public class Main {      
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String word = br.readLine()+" ";                
-        String[] cre1 = word.split("[-=]");  
-        String[] cre2 = word.split("j");
-        int count = word.length() - 1 - croatia(cre1,1) - croatia(cre2,2);
-        System.out.println(count);
+        String word = br.readLine();                         
+        
+        word = word.replace("dz=","1")
+                   .replace("c-","1")
+                   .replace("c=", "1")
+                   .replace("d-", "1")
+                   .replace("lj", "1")
+                   .replace("nj", "1")
+                   .replace("s=", "1")
+                   .replace("z=", "1");        
+        
+        System.out.println(word.length());
     }
 }
