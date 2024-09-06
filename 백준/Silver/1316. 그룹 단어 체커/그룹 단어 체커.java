7d540ae97a54385373c8ff8aee05f5b9794a92d0
송유진 <140707732+Yujin1039@users.noreply.github.com>
2024-09-06 18:48:58 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -5,19 +5,19 @@ public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());  
-        int count = 0;
+        int count = N;
         
         while(N > 0){
-            StringBuilder word = new StringBuilder(br.readLine());
+            String word = br.readLine();
             int[] alphabet = new int[26];
-            count++;
+            char prev = 0;
             for(int i=0;i<word.length();i++){
-                if(i > 0 && word.charAt(i) == word.charAt(i-1)){
-                    continue;
-                }
-                if(++alphabet[word.charAt(i)-97] > 1) {
-                    count--;
-                    break;
+                if(word.charAt(i) != prev){
+                    if(++alphabet[word.charAt(i)-97] > 1) {
+                        count--;
+                        break;
+                    }
+                    prev = word.charAt(i);
                 }
             }
             N--;
