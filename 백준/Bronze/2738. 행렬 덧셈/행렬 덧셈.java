04d47567c4c0d8e11728a38ffd6130904d5dada2
송유진 <140707732+Yujin1039@users.noreply.github.com>
2024-09-07 21:54:00 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 140 ms, Memory: 16832 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -2,36 +2,28 @@ import java.util.*;
 import java.io.*;
 
 public class Main {   
-    static BufferedReader br;
-    static StringTokenizer st;
-    
-    static int[][] setArrays(int h,int w) throws Exception{
-        int[][] arr = new int[h][w];
-        for(int i=0;i<h;i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j=0;j<w;j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        return arr;
-    }
     public static void main(String[] args) throws Exception{
-        br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
             
         int N = Integer.parseInt(st.nextToken());  
         int M = Integer.parseInt(st.nextToken());
-        int[][] A = setArrays(N,M);
-        int[][] B = setArrays(N,M);
+        int[][] sum = new int[N][M];
+
+        for(int i=0;i<N*2;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0;j<M;j++){
+                sum[i%N][j] += Integer.parseInt(st.nextToken());
+            }
+        }
 
         for(int i=0;i<N;i++){
             for(int j=0;j<M;j++){
-                A[i][j] += B[i][j];
-                sb.append(A[i][j]).append(" ");
+                sb.append(sum[i][j]).append(" ");
             }
             sb.append("\n");
-        }         
+        }
         System.out.println(sb);
     }
 }
