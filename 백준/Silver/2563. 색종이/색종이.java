11886304613d877c30aa83282ebd4437a4a96e63
송유진 <140707732+Yujin1039@users.noreply.github.com>
2024-09-10 23:10:22 +0900

    [Silver V] Title: 색종이, Time: 100 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+        int[][] square = new int[100][100];
+        int squareSum = 0;
+		
+		for(int i=0;i<N;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int h = Integer.parseInt(st.nextToken());
+            for(int j=h;j<h+10;j++){
+                for(int k=w;k<w+10;k++){
+                    if(square[j][k] < 1){
+                        squareSum++;
+                        square[j][k]++;
+                    }
+                }
+            }
+        }
+		System.out.println(squareSum);		
+	}
+}
