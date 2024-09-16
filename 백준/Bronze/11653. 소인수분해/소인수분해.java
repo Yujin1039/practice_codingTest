5d65389958e8bed1f8e7a9458c4fe6963b02f340
송유진 <140707732+Yujin1039@users.noreply.github.com>
2024-09-16 16:31:43 +0900

    [Bronze I] Title: 소인수분해, Time: 100 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -2,43 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {	
-    private static List<Integer> primeList = new ArrayList<>();
-    
-    public static void divideNum(int n){
-        if(isPrime(n)) primeList.add(n);
-        else{
-            for(int i = (int) Math.sqrt(n);i<n;i++){
-                if(n % i == 0) {
-                    divideNum(i); 
-                    divideNum(n/i);
-                    break;
-                }                
-            }
-        }        
-    }
-
-    public static boolean isPrime(int m){
-        for(int i=2;i<m;i++){
-            if(m % i == 0) return false;
-        }
-        return true;
-    }
-    
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();        
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
 
-        divideNum(n);
-
-        if(n == 1){
-            System.out.println();    
-        }else{
-            Collections.sort(primeList);
-            for(int j:primeList){
-                sb.append(j).append("\n");   
-            } 
+        for(int i=2;i<=Math.sqrt(n);i++){
+            while(n % i == 0){
+                sb.append(i).append("\n");	
+                n /= i;
+            }
         }
-        System.out.println(sb.toString());
+
+        if(n > 1){
+            sb.append(n).append("\n");
+        }
+            
+    	System.out.println(sb.toString());		            
 	}
 }
