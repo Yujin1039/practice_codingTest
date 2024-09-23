b942b2eabbbfc4762b5b202f51cf9578ad6a5451
송유진 <140707732+Yujin1039@users.noreply.github.com>
2024-09-23 14:41:27 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 30.29 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -2,12 +2,7 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String myString) {
-        String[] str = myString.split("x");        
-        List<String> list = new ArrayList<>();
-        for(String s:str){
-            if(s.length() != 0) list.add(s);
-        }  
-        Collections.sort(list);
-        return list.toArray(String[]::new);
+        String[] str = myString.split("x");                
+        return Arrays.stream(str).filter(s -> !s.equals("")).sorted().toArray(String[]::new);
     }
 }
