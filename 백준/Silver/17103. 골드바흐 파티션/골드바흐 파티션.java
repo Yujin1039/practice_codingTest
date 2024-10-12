fba08164c623fc8d42f577be80ab8fcac14851e6
송유진 <140707732+Yujin1039@users.noreply.github.com>
2024-10-12 23:31:28 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 160 ms, Memory: 15456 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -3,14 +3,16 @@ import java.io.*;
 
 public class Main {
     static boolean[] prime = new boolean[1000001];
-    public static int isPrimes(int a){
-        int b = 0;        
-        for(int i=2;i<=Math.sqrt(a);i++){
+    public static void getPrime(){
+        for(int i=2;i<=Math.sqrt(1000001);i++){
             if(prime[i]) continue;
-            for(int j=i*2;j<=a;j += i){
+            for(int j=i*2;j<=1000000;j += i){
                 prime[j] = true;
             }
         }
+    }
+    public static int isPrimes(int a){        
+        int b = 0;                        
         if(a == 4) return 1;
         for(int i=3;i<=a/2;i+=2){
             if(!prime[i] && !prime[a - i]){
@@ -23,8 +25,9 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        StringBuilder sb = new StringBuilder();
         prime[0] = prime[1] = true;
+        getPrime();
+        StringBuilder sb = new StringBuilder();
         for(int i=0;i<n;i++){
             int m = Integer.parseInt(br.readLine());
             sb.append(isPrimes(m)).append("\n");
