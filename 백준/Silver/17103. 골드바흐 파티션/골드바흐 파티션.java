ca7b74395af28b5778bc4becb6093be87033080e
송유진 <140707732+Yujin1039@users.noreply.github.com>
2024-10-12 23:27:28 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 252 ms, Memory: 15736 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -1,19 +1,18 @@
 import java.util.*;
-import java.lang.*;
 import java.io.*;
 
 public class Main {
+    static boolean[] prime = new boolean[1000001];
     public static int isPrimes(int a){
-        boolean[] prime = new boolean[a+1];
         int b = 0;        
-        prime[0] = prime[1] = true;
         for(int i=2;i<=Math.sqrt(a);i++){
             if(prime[i]) continue;
             for(int j=i*2;j<=a;j += i){
                 prime[j] = true;
             }
         }
-        for(int i=2;i<=a/2;i++){
+        if(a == 4) return 1;
+        for(int i=3;i<=a/2;i+=2){
             if(!prime[i] && !prime[a - i]){
                 b++;
             } 
@@ -25,6 +24,7 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
 
         StringBuilder sb = new StringBuilder();
+        prime[0] = prime[1] = true;
         for(int i=0;i<n;i++){
             int m = Integer.parseInt(br.readLine());
             sb.append(isPrimes(m)).append("\n");
