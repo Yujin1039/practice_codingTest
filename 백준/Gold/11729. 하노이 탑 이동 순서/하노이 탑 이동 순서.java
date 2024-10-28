96b8a1295af9fd99857a3e28c2cf576271908f5f
송유진 <140707732+Yujin1039@users.noreply.github.com>
2024-10-28 22:41:36 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 476 ms, Memory: 67996 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static StringBuilder sb;
+    public static void order(int x,int y,int n){
+        if(n == 1) {
+            sb.append(x+" "+y+"\n");
+            return;
+        }
+        order(x,6-x-y,n-1);
+        sb.append(x+" "+y+"\n");  
+        order(6-x-y,y,n-1); 
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        sb = new StringBuilder();
+        
+        order(1,3,n);
+        System.out.println((int) Math.pow(2,n) -1);
+        System.out.println(sb.toString());
+    }
+}
