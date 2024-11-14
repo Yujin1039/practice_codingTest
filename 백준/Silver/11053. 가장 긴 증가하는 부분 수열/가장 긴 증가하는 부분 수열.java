333560597cc13d1ca1df92212600832e208dddf5
송유진 <140707732+Yujin1039@users.noreply.github.com>
2024-11-14 21:24:22 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 124 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -14,18 +14,15 @@ public class Main {
             num[i] = Integer.parseInt(st.nextToken());
         }
         
+        int answer = 1;
         for(int i=1;i<n;i++){
             for(int j=i;j>0;j--){
-                if(num[i] > num[j-1]){
-                    count[i] = Math.max(count[i],count[j-1] + 1);
+                if(num[i] > num[j-1] && count[i] < count[j-1] + 1){
+                    count[i] = count[j-1] + 1;
                 }
             }
             if(count[i] == 0) count[i] = 1;
-        }
-
-        int answer = 0;
-        for(int cnt:count){
-            if(answer < cnt) answer = cnt;
+            if(answer < count[i]) answer = count[i];
         }
         System.out.println(answer);
     }
