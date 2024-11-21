0a8f10640dd1bfa7e76a245fbc5e3153a8a8898d
송유진 <140707732+Yujin1039@users.noreply.github.com>
2024-11-22 01:30:03 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1156 ms, Memory: 144796 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,22 +1,40 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {    
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
-        StringBuilder sb = new StringBuilder();
-        int a = Integer.parseInt(br.readLine());     
-        List<Integer> list = new ArrayList<>();
-		for(;a>0;a--){
-            int b = Integer.parseInt(br.readLine());     
-            list.add(b);
-        }
-        Collections.sort(list);
+public class Main {
+    static int[] arr;
+    static int[] tmp; 
 
-        for(int c: list){
-            sb.append(c).append("\n");
+    static void merge(int st, int en){     
+        int mid = (st+en)/2;
+        int tM = mid; int tS = st;
+        for(int i=st;i<en;i++){
+            if(tM == en) tmp[i] = arr[tS++];
+            else if(tS == mid) tmp[i] = arr[tM++];
+            else if(arr[tS] >= arr[tM]) tmp[i] = arr[tM++];
+            else tmp[i] = arr[tS++];
         }
+        for(int i=st;i<en;i++) arr[i] = tmp[i];
+    }
 
-        System.out.println(sb);
-	}
+    static void merge_sort(int st, int en){
+        if(st+1 == en) return;
+        int mid = (st+en)/2;
+        merge_sort(st, mid);
+        merge_sort(mid, en);
+        merge(st, en);
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n];
+        tmp = new int[n];
+        
+        for(int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        merge_sort(0, n);
+        System.out.println(Arrays.toString(arr).replace("[","").replace("]","").replace(", ","\n"));
+    }
 }
