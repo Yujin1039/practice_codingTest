5b3fc4d40a1c5a7f4076f1d7e9d69849d28ade09
송유진 <140707732+Yujin1039@users.noreply.github.com>
2024-11-27 20:59:27 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main { 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[] fibo = new long[n+1];
+
+        fibo[0] = 0; fibo[1] = 1;
+        for(int i=2;i<=n;i++){
+            fibo[i] = fibo[i-2] + fibo[i-1];
+        }
+        System.out.println(fibo[n]);
+    }
+}
