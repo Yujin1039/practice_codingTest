79e2cecc3c61f894a0ee2ed03c23cb596033ac6e
송유진 <140707732+Yujin1039@users.noreply.github.com>
2025-01-25 22:19:53 +0900

    [Gold III] Title: 공주님의 정원, Time: 424 ms, Memory: 54404 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.java
@@ -7,8 +7,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
         int n = Integer.parseInt(br.readLine()); 
         
-        boolean[][] isBloom = new boolean[n][366];
-        int[] duration = new int[n]; 
+        int[][] isBloom = new int[n][2]; 
 
         for(int i=0;i<n;i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -18,14 +17,12 @@ public class Main {
             int ed = Integer.parseInt(st.nextToken());
 
             int sIdx = LocalDate.of(2025,sm,sd).getDayOfYear()-1;
-            int eIdx = LocalDate.of(2025,em,ed).getDayOfYear()-1;
+            int eIdx = LocalDate.of(2025,em,ed).getDayOfYear()-2;
 
-            for(int j=sIdx;j<eIdx;j++){
-                isBloom[i][j] = true;
-            }
-            duration[i] = eIdx-1;
+            isBloom[i][0] = sIdx;
+            isBloom[i][1] = eIdx;
         }
-
+               
         int start = LocalDate.of(2025,3,1).getDayOfYear() -1;
         int end = LocalDate.of(2025,11,30).getDayOfYear() -1;
         int flowers = 0;
@@ -34,9 +31,9 @@ public class Main {
             int num = 0;
             int max = 0;
             for(int j=0;j<n;j++){
-                if(isBloom[j][i]) {
+                if(i >= isBloom[j][0] && i <= isBloom[j][1]) {
                     num++;
-                    max = Math.max(max,duration[j]);
+                    max = Math.max(max,isBloom[j][1]);
                 }                
             }
             if(num == 0) {
