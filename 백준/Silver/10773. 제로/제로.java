f1fbde9ac031566f40a4c475fd91bb0e4d3a6610
송유진 <140707732+Yujin1039@users.noreply.github.com>
2025-11-06 21:03:14 +0900

    [Silver IV] Title: 제로, Time: 204 ms, Memory: 22992 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,27 +1,24 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main { 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //StringBuilder sb = new StringBuilder();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine());
-        Stack<Integer> money = new Stack<>();
-        int sum = 0;
-        
-        for(int i=0;i<N;i++){
+        int size = Integer.parseInt(br.readLine());
+        int[] stack = new int[size];
+        int index = 0;
+
+        for(int i=0; i<size; i++){
             int num = Integer.parseInt(br.readLine());
+
             if(num == 0){
-                money.pop();
-            }else{
-                money.push(num);
+                stack[--index] = 0;
+            } else{
+                stack[index++] = num;
             }
         }
-
-        while(!money.isEmpty()){
-            sum += money.pop();
-        }
-        System.out.println(sum);
-	}    
+        System.out.println(Arrays.stream(stack).sum());
+    }
 }
