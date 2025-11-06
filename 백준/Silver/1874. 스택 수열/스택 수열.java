eb0383fcde4eecfc2d99c01cf757cd8083d43d33
송유진 <140707732+Yujin1039@users.noreply.github.com>
2025-11-06 20:46:42 +0900

    [Silver II] Title: 스택 수열, Time: 284 ms, Memory: 26976 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,39 +1,39 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {     
-    public static String isSequence(int[] seq){
-        ArrayDeque<Integer> stack = new ArrayDeque<>();
-        StringBuilder sb = new StringBuilder();
-        int pre = 0;
-        for(int i=0;i<seq.length;i++){
-            int num = stack.isEmpty() ? 1:stack.peekLast();
-            if(num == seq[i]){
-                if(stack.isEmpty()) sb.append("+\n");
-                else stack.pollLast();
-                sb.append("-\n");
-            }else if(num < seq[i]){
-                for(int j=pre+1;j<=seq[i];j++){
-                    stack.offer(j);
-                    sb.append("+\n");
-                }
-                stack.pollLast();
-                sb.append("-\n");
-            }else{
-                return "NO";
-            }
-            pre = pre > seq[i] ? pre:seq[i];
-        }
-        return sb.deleteCharAt(sb.length()-1).toString();
-    }
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] expectedSeq = new int[n];
- 
-        for(int i=0;i<n;i++){
-            expectedSeq[i] = Integer.parseInt(br.readLine());
-        }                
-        System.out.println(isSequence(expectedSeq));
+        StringBuilder sb = new StringBuilder();
+
+        int size = Integer.parseInt(br.readLine())+1;
+        int prev = 1;
+        int[] stack = new int[size];
+
+        for(int i=0; i<size-1; i++){
+            int num = Integer.parseInt(br.readLine());
+            int cur = prev;
+
+            while(cur <= num){
+                if(stack[cur] == 0) {
+                    stack[cur] = cur;
+                    sb.append("+\n");
+                } 
+                cur++;    
+            }
+            
+            while(cur > num){
+                cur--;
+                if(stack[cur] == num) {
+                    stack[cur] = -1;
+                    sb.append("-\n");
+                } else if(stack[cur] >  num){
+                    System.out.println("NO");
+                    System.exit(0);
+                }
+            } 
+            prev = num;
+        }
+        System.out.println(sb.toString());
     }
 }
