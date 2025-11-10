28e3684098983bb1e73f93f394af6a76bd7ec346
송유진 <140707732+Yujin1039@users.noreply.github.com>
2025-11-10 20:21:30 +0900

    [Silver II] Title: 촌수계산, Time: 100 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -2,7 +2,8 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int members;
+    //static int[][] xy = {{1, 0, -1, 0}, {0, 1, 0, -1}}; 
+    static int members, personE;
     static int[][] family;
     static int[][] distance;
     static boolean[] isVisited;
@@ -17,7 +18,7 @@ public class Main {
         
         StringTokenizer st = new StringTokenizer(br.readLine()); 
         int personS = Integer.parseInt(st.nextToken());
-        int personE = Integer.parseInt(st.nextToken());
+        personE = Integer.parseInt(st.nextToken());
         
         int n = Integer.parseInt(br.readLine());
         
@@ -32,7 +33,12 @@ public class Main {
         queue = new LinkedList<>();
         queue.add(new int[]{personS,0});
         isVisited[personS] = true;
+
+        System.out.println(bfs());
         
+    }
+    
+    private static int bfs(){
         while(!queue.isEmpty()){
             int[] temp = queue.poll();
             int personN = temp[0];
@@ -41,8 +47,7 @@ public class Main {
             for(int i=1; i<members+1; i++){
                 if(!isVisited[i] && family[personN][i] == 1){
                     if(i == personE){
-                        System.out.println(kinship+1);
-                        System.exit(0);
+                        return kinship+1;
                     }
                     distance[personN][i] = kinship+1;
                     distance[i][personN] = kinship+1;
@@ -50,15 +55,7 @@ public class Main {
                     queue.add(new int[]{i,kinship+1});
                 }
             }
-        }
-
-        
-        for(int i=0; i<members+1; i++){
-            if(family[personE][i] == 0){
-                System.out.println(-1);
-                System.exit(0);
-            } 
-        }
-        
-    }
+        } 
+        return -1;
+    } 
 }
