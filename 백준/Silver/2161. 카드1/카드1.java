a1cb123e0fa5a414c90d1f644a9503ff9898f155
송유진 <140707732+Yujin1039@users.noreply.github.com>
2025-11-14 04:57:54 +0900

    [Silver V] Title: 카드1, Time: 112 ms, Memory: 14568 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -0,0 +1,26 @@
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> cards = new LinkedList<>();
+
+        for(int i=1; i<n+1; i++){
+            cards.offer(i);
+        }
+
+        while (cards.size() > 1) {
+            sb.append(cards.poll()+" ");
+            cards.offer(cards.poll());
+        }
+        sb.append(cards.poll());
+        
+        System.out.println(sb.toString());
+    }
+}
