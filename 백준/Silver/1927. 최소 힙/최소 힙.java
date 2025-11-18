8feaa57c5c983a4e0b95afbaf2652b654ef122f8
송유진 <140707732+Yujin1039@users.noreply.github.com>
2025-11-19 05:06:23 +0900

    [Silver II] Title: 최소 힙, Time: 288 ms, Memory: 26148 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<n;i++){
+            int num = Integer.parseInt(br.readLine());
+            if(num == 0){ 
+                int pop = (pq.size() == 0) ? 0:pq.poll();
+                sb.append(pop).append("\n");
+            }else{
+                pq.add(num);
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
