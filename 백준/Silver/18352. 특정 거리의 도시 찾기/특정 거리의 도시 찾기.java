9d697009a929c165261a0e1a9af7e8bb25e05bcf
송유진 <140707732+Yujin1039@users.noreply.github.com>
2025-12-10 05:08:40 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1140 ms, Memory: 269700 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {    
+    static int INF = 300_001;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st =  new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        int[] dis = new int[N+1]; // 최단거리 배열
+        Arrays.fill(dis,INF);
+
+        List<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i=0; i<=N; i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0; i<M; i++){
+            st =  new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            adj.get(start).add(end);
+        }
+
+        PriorityQueue<int[]> queue = new PriorityQueue<>((o1,o2) -> o1[0]-o2[0]);
+        dis[X] = 0;
+        queue.add(new int[]{0,X});
+        
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int start = cur[1];
+
+            for(int i=0; i<adj.get(start).size(); i++){
+                if(dis[adj.get(start).get(i)] > cur[0]+1){
+                    dis[adj.get(start).get(i)] = cur[0]+1;
+                    queue.add(new int[]{cur[0]+1,adj.get(start).get(i)});
+                }
+            }  
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<dis.length; i++){
+            if(dis[i] == K) sb.append(i+"\n");
+        }
+        
+        System.out.println(sb.toString().isEmpty() ? "-1":sb.toString());
+    }
+}
