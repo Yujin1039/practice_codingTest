38ac9b310fed25eb5a27231ef584537a5ba2e2f0
송유진 <140707732+Yujin1039@users.noreply.github.com>
2025-12-14 15:12:48 +0900

    [Silver IV] Title: 로프, Time: 268 ms, Memory: 25472 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -1,22 +1,21 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] rope = new int[n];
-        
-        for(int i=0;i<n;i++){
+        int N = Integer.parseInt(br.readLine());
+        int[] rope = new int[N];
+
+        for(int i=0; i<N; i++){
             rope[i] = Integer.parseInt(br.readLine());
         }
-
         Arrays.sort(rope);
+
         int max = 0;
-        for(int i=n-1;i>=0;i--){
-            int next = rope[i]*(n-i);
-            if(max < next) max = next;                
-        } 
+        for(int i=0; i<N; i++){
+            max = Math.max(rope[i]*(N-i),max);
+        }
         System.out.println(max);
     }
 }
