9f16d84f1a118b47f8280e9c5592af406cfb904f
송유진 <140707732+Yujin1039@users.noreply.github.com>
2026-01-18 19:39:19 +0900

    [Gold II] Title: 반도체 설계, Time: 3004 ms, Memory: 20492 KB -BaekjoonHub

--- 백준/Gold/2352. 반도체 설계/반도체 설계.java
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.java
@@ -12,18 +12,19 @@ public class Main {
             pNum[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[][] conNum = new int[n+1][2];
-        conNum[1][1] = 1;
-        for (int i=2; i<=n; i++) {
-            conNum[i][0] = Math.max(conNum[i-1][0], conNum[i-1][1]);
-            conNum[i][1] = 1;
-            
-            for(int j=i-1; j>0; j--) {
+        int[] conNum = new int[n+1];
+        Arrays.fill(conNum, 1);
+        for (int i=2; i<=n; i++) {            
+            for(int j=i-1; j>0; j--) { 
                 if(pNum[j] < pNum[i]) {
-                    conNum[i][1] = Math.max(conNum[j][1]+1, conNum[i][1]);  
+                    conNum[i] = Math.max(conNum[j]+1, conNum[i]);  
                 }
             }
         }
-        System.out.println(Math.max(conNum[n][0], conNum[n][1]));
+        int ans = 0;
+        for (int num : conNum) {
+            ans = Math.max(num,ans);
+        }
+        System.out.println(ans);
     }
 }
