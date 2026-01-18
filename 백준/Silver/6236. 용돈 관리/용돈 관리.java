23c2f87c1ea48872d3d80489ece3ecc3e5319b94
송유진 <140707732+Yujin1039@users.noreply.github.com>
2026-01-18 16:49:22 +0900

    [Silver I] Title: 용돈 관리, Time: 232 ms, Memory: 22832 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.java
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -1,39 +1,41 @@
 import java.util.*;
 import java.io.*;
-import java.util.Collections.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main {    
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
-        int[] expense = new int[N];
-        long min = 0; long max = 0;
+        
+        int[] budget = new int[N];
+        int min = 0; int max = 0;
         for(int i=0; i<N; i++){
-            int exp = Integer.parseInt(br.readLine());
-            expense[i] = exp;
-            min = Math.max(min, exp);
-            max += exp;
+            int bd = Integer.parseInt(br.readLine());
+            budget[i] = bd;
+            min = Math.max(min, bd);
+            max += bd;
         }
 
-        while(min < max){
-            long K = (min+max)/2;
+        for(int i=0; i<N; i++){
+            while(max > min){               
+                int mid = (max+min+1)/2;
 
-            int cnt = 1; long tmp = K;
-            for(int exp:expense){
-                if(tmp >= exp) tmp -= exp;
-                else {
-                    cnt++;                    
-                    tmp = (K-exp);
+                int num = 0; int curBd = 0;
+                for (int bd : budget) {
+                    if(bd >= curBd){
+                        num++;
+                        curBd = mid;
+                    }
+                    curBd -= bd;
                 }
-            }
-
-            if(cnt > M){
-                min = K+1;
-            } else {
-                max = K;
+                
+                if(num > M){
+                    min = mid;
+                } else {
+                    max = mid-1;
+                }
             }
         }
         System.out.println(min);
