a03b087ce759d285ea2a228b159e1dd0e14d9493
송유진 <140707732+Yujin1039@users.noreply.github.com>
2026-01-18 20:06:37 +0900

    [Gold II] Title: 반도체 설계, Time: 224 ms, Memory: 20264 KB -BaekjoonHub

--- 백준/Gold/2352. 반도체 설계/반도체 설계.java
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.java
@@ -6,25 +6,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         
-        int[] pNum = new int[n+1];
+        int[] pNum = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i=1; i<=n; i++) {
+        for (int i=0; i<n; i++) {
             pNum[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] conNum = new int[n+1];
-        Arrays.fill(conNum, 1);
-        for (int i=2; i<=n; i++) {            
-            for(int j=i-1; j>0; j--) { 
-                if(pNum[j] < pNum[i]) {
-                    conNum[i] = Math.max(conNum[j]+1, conNum[i]);  
+        int[] conNum = new int[n];
+        int idx = 1; 
+        conNum[0] = pNum[0];
+
+        for (int i=1; i<n; i++) {            
+            if(conNum[idx-1] < pNum[i]){
+                conNum[idx++] = pNum[i];
+            } else {
+                int mIdx = Arrays.binarySearch(conNum, 0, idx, pNum[i]);
+                if(mIdx < 0){
+                    mIdx = -(mIdx+1);                    
                 }
+                conNum[mIdx] = pNum[i];
             }
         }
-        int ans = 0;
-        for (int num : conNum) {
-            ans = Math.max(num,ans);
-        }
-        System.out.println(ans);
+        System.out.println(idx);
     }
 }
