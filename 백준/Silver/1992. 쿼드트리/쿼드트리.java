342bad335a78c384f685e72c9ff7fe05e201e323
송유진 <140707732+Yujin1039@users.noreply.github.com>
2026-03-18 22:04:48 +0900

    [Silver I] Title: 쿼드트리, Time: 108 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,50 +1,49 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main { 
-    static int[][] videos;
-    static int n;
-    static StringBuilder sb = new StringBuilder();
-    
-    static void quadTree(int[] xy,int sub){
-        int start = videos[xy[0]][xy[1]];
-        if(sub == 1) {
-            sb.append(start); 
-            return;
-        }
-        boolean isSame = true;
+public class Main {    
+    static int[][] video;
+    static StringBuilder densedStr;
 
-        fin:
-        for(int i=xy[0];i<xy[0]+sub;i++){
-            for(int j=xy[1];j<xy[1]+sub;j++){
-                if(videos[i][j] != start){
-                    sb.append("(");
-                    isSame = false;
-                    int num = sub/2;          
-                    quadTree(new int[]{xy[0],xy[1]},num);
-                    quadTree(new int[]{xy[0],xy[1]+num},num);
-                    quadTree(new int[]{xy[0]+num,xy[1]},num);
-                    quadTree(new int[]{xy[0]+num,xy[1]+num},num);
-                    sb.append(")");
-                    break fin;
+    static void quadTree(int x, int y, int n, int dir){
+        boolean isQuad = true;
+        int start = video[x][y];
+        loop:
+        for(int i=x; i<x+n; i++){
+            for(int j=y; j<y+n; j++){
+                if(video[i][j] != start){
+                    isQuad = false;
+                    break loop;
                 }
             }
         }
-        if(isSame) sb.append(start);
+        
+        if(isQuad) {            
+            densedStr.append(start);
+        } else {
+            densedStr.append("(");
+            quadTree(x, y, n/2, 1);                    
+            quadTree(x, y+n/2, n/2, 2);
+            quadTree(x+n/2, y, n/2, 3);
+            quadTree(x+n/2, y+n/2, n/2, 4); 
+            densedStr.append(")");
+        }
     }
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        videos = new int[n][n];
-
-        for(int i=0;i<n;i++){
+        int N = Integer.parseInt(br.readLine());
+        
+        video = new int[N][N];        
+        for(int i=0; i<N; i++){
             String str = br.readLine();
-            for(int j=0;j<n;j++){
-                videos[i][j] = str.charAt(j)-48;
+            for(int j=0; j<N; j++){
+                video[i][j] = str.charAt(j) - '0';
             }
         }
 
-        quadTree(new int[]{0,0},n);
-        System.out.println(sb);
+        densedStr = new StringBuilder();
+        quadTree(0, 0, N, 1);
+        System.out.println(densedStr.toString());
     }
 }
