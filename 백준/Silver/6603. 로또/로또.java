2a02be07abef0d7b31fc4de8f4f249c5160520e8
송유진 <140707732+Yujin1039@users.noreply.github.com>
2026-03-19 06:10:50 +0900

    [Silver II] Title: 로또, Time: 108 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,37 +1,50 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {   
-    static int k;
-    static int[] s;
-    static int[] tmp = new int[6];
-    static StringBuilder sb = new StringBuilder();
+public class Main {  
+    static StringBuilder sb;
+    static int[] arr;
+    static boolean[] isVisited;
+    static int[] lotto;
 
-    static void lotto(int sIdx, int tIdx){
-        if(tIdx == 6){
-            for(int t:tmp){
-                sb.append(t).append(" ");
+    static void findComb(int start, int n, int[] lotto){
+        if(n == 6){
+            for(int i=0; i<6; i++){
+                sb.append(lotto[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
-        for(int i=sIdx;i<k;i++){
-            tmp[tIdx] = s[i];
-            lotto(i+1,tIdx+1);
+        for (int i=start; i<arr.length; i++) {
+            if(!isVisited[i]){
+                isVisited[i] = true;
+                lotto[n] = arr[i];
+                findComb(i+1, n+1, lotto);
+                isVisited[i] = false;
+            }
         }
     }
     public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        do{
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            k = Integer.parseInt(st.nextToken());
-            s = new int[k];
-            for(int i=0;i<k;i++){
-                s[i] = Integer.parseInt(st.nextToken());               
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String cmd = br.readLine();
+        sb = new StringBuilder();
+
+        while (!cmd.equals("0")) {
+            StringTokenizer st = new StringTokenizer(cmd);
+            
+            int n = Integer.parseInt(st.nextToken());
+            arr = new int[n];
+            for(int i=0; i<n; i++){
+                arr[i] = Integer.parseInt(st.nextToken());
             }
-            lotto(0,0);
+            isVisited = new boolean[n];
+            lotto = new int[6];
+            findComb(0, 0, lotto);
             sb.append("\n");
-        }while(k != 0);
-        System.out.println(sb);
-	}
+            
+            cmd = br.readLine();
+        }
+
+        System.out.println(sb.toString());
+    }
 }
