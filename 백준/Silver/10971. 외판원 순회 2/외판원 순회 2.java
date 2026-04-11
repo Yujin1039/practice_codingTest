75f3a388dfa8009112e3e2956e891f3d9394c2d1
송유진 <140707732+Yujin1039@users.noreply.github.com>
2026-04-12 08:23:09 +0900

    [Silver II] Title: 외판원 순회 2, Time: 160 ms, Memory: 14760 KB -BaekjoonHub

---
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N;
+    static int[][] info;
+    static int cost;
+    static boolean[] from;
+    static boolean[] to;
+
+    static void dfs(int prev, int cnt, int curCost){
+        if(cnt == N){
+            cost = Math.min(cost, curCost);
+            return;
+        }
+        if(from[prev]) return;
+        
+        for(int i=0; i<N; i++){
+            if(i == prev || info[prev][i] == 0 || to[i]) continue;
+            from[prev] = true;
+            to[i] = true;            
+            dfs(i, cnt+1, curCost+info[prev][i]);
+            from[prev] = false;
+            to[i] = false;
+        }
+    }
+    
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        info = new int[N][N];
+        cost = 10_000_001;
+        for(int i=0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++){
+                info[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        from = new boolean[N];
+        to = new boolean[N];
+        dfs(0, 0, 0);      
+
+        System.out.println(cost);
+    }
+}
