be6b9761071a19a82cd771349c57d0559a5eb18d
송유진 <140707732+Yujin1039@users.noreply.github.com>
2026-04-13 08:38:42 +0900

    [Gold II] Title: 보석 도둑, Time: 1692 ms, Memory: 123500 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,46 +1,43 @@
 import java.util.*;
 import java.io.*;
-import java.util.Collections.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main {   
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
         
-        int N = Integer.parseInt(st.nextToken()); // 보석점 수
-        int K = Integer.parseInt(st.nextToken()); // 가방 수
-
-        int[][] jewels = new int[N][2];        
+        PriorityQueue<int[]> jewel = new PriorityQueue<>((o1,o2)->{
+            if(o1[0] == o2[0]){
+                return o2[1]-o1[1];
+            }
+            return o1[0]-o2[0];
+        });
+        
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
-            int M = Integer.parseInt(st.nextToken());
-            int V = Integer.parseInt(st.nextToken());
-
-            jewels[i][0] = M;
-            jewels[i][1] = V;
+            int m = Integer.parseInt(st.nextToken()); // 무게
+            int v = Integer.parseInt(st.nextToken()); // 가치
+            jewel.add(new int[]{m,v});
         }
-        Arrays.sort(jewels, (arr1, arr2) -> Integer.compare(arr1[0],arr2[0]));
-
-        int[] bags = new int[K];
+        
+        Integer[] bag = new Integer[K];
         for(int i=0; i<K; i++){
-            bags[i] = Integer.parseInt(br.readLine());
+            bag[i] = Integer.parseInt(br.readLine()); // 최대 저장 무게
         }
-        Arrays.sort(bags);
+        Arrays.sort(bag);
 
-        long value = 0;
-        PriorityQueue<Integer> info = new PriorityQueue<>(Collections.reverseOrder());
-        int idx = 0;
+        long maxValue = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1,o2)->o2-o1);
         for(int i=0; i<K; i++){
-            for(int j=idx; j<N; j++){                
-                if(bags[i] >= jewels[j][0]){
-                    info.offer(jewels[j][1]);
-                } else {
-                    break;
-                }
-              idx++;
+            while (!jewel.isEmpty()) {            
+                if(jewel.peek()[0] > bag[i]) break;               
+                pq.add(jewel.poll()[1]);
             }            
-            if(!info.isEmpty()) value += info.poll();
+            maxValue += pq.isEmpty() ? 0:pq.poll();
         }
-        System.out.println(value);        
+
+        System.out.println(maxValue);
     }
 }
