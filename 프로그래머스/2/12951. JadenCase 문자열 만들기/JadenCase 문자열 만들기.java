6a061ed7bcaad6d0471513c173e1e41b6d274df6
송유진 <140707732+Yujin1039@users.noreply.github.com>
2026-05-04 22:24:23 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 2.91 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,19 +1,10 @@
-import java.util.*;
+import java.util.regex.*;
 
 class Solution {
     public String solution(String s) {
         s = s.toLowerCase();
-        StringBuilder sb = new StringBuilder();
-        sb.append(s.substring(0,1).toUpperCase());
         
-        for(int i=1; i<s.length(); i++){
-            if(s.charAt(i-1) == ' '){
-                sb.append(s.substring(i,i+1).toUpperCase());
-            } else {
-                sb.append(s.charAt(i));
-            }
-        }
-
-        return sb.toString();
+        return Pattern.compile("(^| )(\\S)").matcher(s)
+                    .replaceAll(m -> m.group(1)+m.group(2).toUpperCase());
     }
 }
